db14fc5909d41fde490420eb74340edf9fb16465
shadwoe <rudinnawi@gmail.com>
2020-10-13 15:31:46 +0700

    add mybag page

--- src/App.js
+++ src/App.js
@@ -3,23 +3,25 @@ import {BrowserRouter as Router, Route} from 'react-router-dom';
 import './App.css';
 
 import Home from './pages/Home';
-// import Profile from './pages/Profile';
+import Profile from './pages/Profile';
 import PageProduct from './pages/page-product/pageProduct';
 import CategoryPage from './pages/category-page/categoryPage';
 import Checkout from './pages/checkout/checkout';
 import Chat from './pages/chatting/chatting';
+import MyBag from './pages/my-bag/myBag';
 
 
 
 function App() {
   return (
     <Router>
-//       <Profile/>
+      <Route path='/profile' exact component={Profile} />
       <Route path='/' exact component={Home} />
       <Route path='/product' exact component={PageProduct} />
       <Route path='/category' exact component={CategoryPage} />
       <Route path='/checkout' exact component={Checkout} />
       <Route path='/chat' exact component={Chat} />
+      <Route path='/mybag' exact component={MyBag} />
     </Router>
   );
 }
